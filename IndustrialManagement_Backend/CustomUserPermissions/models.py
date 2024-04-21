b67eb59772e313f7dea9778cc7e39918b5d2cd37
A_Ariannejad <ariannejad14@gmail.com>
2024-04-21 18:50:14 +0330

    Update models.py

--- IndustrialManagement_Backend/CustomUserPermissions/models.py
+++ IndustrialManagement_Backend/CustomUserPermissions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class CustomUserPermission(models.Model):
+    name = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    is_controller = models.BooleanField(default=False)
+    is_viewer = models.BooleanField(default=False)
+    is_calculator = models.BooleanField(default=False)
+    is_supporter = models.BooleanField(default=False)
