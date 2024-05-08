6330c38c947dce646b9f59cf319cb2062e317bd4
A_Ariannejad <ariannejad14@gmail.com>
2024-05-08 20:50:43 +0330

    Update admin.py

--- IndustrialManagement_Backend/CustomUsers/admin.py
+++ IndustrialManagement_Backend/CustomUsers/admin.py
@@ -3,6 +3,6 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'create_date')
+    list_display = ('id', 'email', 'social_id', 'create_date')
 
 admin.site.register(CustomUser, CustomUserAdmin)
