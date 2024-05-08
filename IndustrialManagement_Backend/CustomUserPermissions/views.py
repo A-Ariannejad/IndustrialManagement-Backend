f6a1c686e2bc10c5da1c5afd759cbdfb9529ef39
A_Ariannejad <ariannejad14@gmail.com>
2024-05-08 16:41:21 +0330

    Custom User is made and ready to be merged

--- IndustrialManagement_Backend/CustomUserPermissions/views.py
+++ IndustrialManagement_Backend/CustomUserPermissions/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+from rest_framework.permissions import BasePermission
+from CustomUsers.models import LogicUser, CustomUser
+from rest_framework import generics, viewsets, status
+from .serializers import PermissionSerializer
+from .models import CustomUserPermission
+from IndustrialManagement_Backend.serializers import CustomValidation
 
-# Create your views here.
+class IsAddSubOrganization(BasePermission):
+    def has_permission(self, request, view):
+        user = LogicUser.get_user(request = request)
+        if user:
+            if user.user_permissions.add_subOrganization:
+                return True
+        return False
+
+class IsAddManager(BasePermission):
+    def has_permission(self, request, view):
+        user = LogicUser.get_user(request = request)
+        if user:
+            if user.user_permissions.add_manager:
+                return True
+        return False
+    
+class IsAddProject(BasePermission):
+    def has_permission(self, request, view):
+        user = LogicUser.get_user(request = request)
+        if user:
+            if user.user_permissions.add_project:
+                return True
+        return False
+    
+    
+class IsView(BasePermission):
+    def has_permission(self, request, view):
+        user = LogicUser.get_user(request = request)
+        if user:
+            if user.user_permissions.view:
+                return True
+        return False
+
+class PermissionShowView(generics.RetrieveAPIView):
+    queryset = CustomUserPermission.objects.all()
+    serializer_class = PermissionSerializer
+    # permission_classes = [IsEditUser]
+    lookup_field = 'id'
+
+class PermissionViewSet(viewsets.ModelViewSet):
+    queryset = CustomUserPermission.objects.all()
+    serializer_class = PermissionSerializer
+    # permission_classes = [IsEditUser]
+
+class PermissionCreateView(generics.CreateAPIView):
+    queryset = CustomUserPermission.objects.all()
+    serializer_class = PermissionSerializer
+    # permission_classes = [IsEditUser]
+
+    def perform_create(self, serializer):
+        instance = serializer.instance
+        if instance.name == 'User':
+            raise CustomValidation("User is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        if instance.name == 'SuperAdmin':
+            raise CustomValidation("SuperAdmin is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        if serializer.validated_data['name'] == 'User':
+            raise CustomValidation("User is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        if serializer.validated_data['name'] == 'SuperAdmin':
+            raise CustomValidation("SuperAdmin is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        return super().perform_create(serializer)
+
+class PermissionUpdateView(generics.UpdateAPIView):
+    queryset = CustomUserPermission.objects.all()
+    serializer_class = PermissionSerializer
+    # permission_classes = [IsEditUser]
+
+    def perform_update(self, serializer):
+        instance = serializer.instance
+        if instance.name == 'User':
+            raise CustomValidation("User is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        if instance.name == 'SuperAdmin':
+            raise CustomValidation("SuperAdmin is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        if serializer.validated_data['name'] == 'User':
+            raise CustomValidation("User is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        if serializer.validated_data['name'] == 'SuperAdmin':
+            raise CustomValidation("SuperAdmin is reserved", "", status_code=status.HTTP_400_BAD_REQUEST)
+        return super().perform_update(serializer)
+
+class PermissionDeleteView(generics.DestroyAPIView):
+    queryset = CustomUserPermission.objects.all()
+    serializer_class = PermissionSerializer
+    # permission_classes = [IsEditUser]
+
+    def perform_destroy(self, instance):
+        if instance.name == 'User':
+            raise CustomValidation("User can not be deleted", "", status_code=status.HTTP_400_BAD_REQUEST)
+        if instance.name == 'SuperAdmin':
+            raise CustomValidation("SuperAdmin can not be deleted", "", status_code=status.HTTP_400_BAD_REQUEST)
+        permission = CustomUserPermission.objects.filter(name='User').first()
+        CustomUser.objects.filter(user_permissions__name=instance.name).all().update(user_permissions__name=permission)
+        return super().perform_destroy(instance)
+
--- IndustrialManagement_Backend/CustomUsers/urls.py
+++ IndustrialManagement_Backend/CustomUsers/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import CreateView, LoginView, CustomUserViewSet, CustomUserUpdateView, CustomUserShowView, CustomUserDeleteView, MyCustomUserShowView, ForgetSendPasswordResetView, ForgetGetPasswordResetView
-from .views import CustomUserChangePasswordView, CustomUserUpdatePermissionView
+from .views import CreateView, CustomUserViewSet, CustomUserUpdateView, CustomUserShowView, CustomUserDeleteView, MyCustomUserShowView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,13 +9,9 @@ urlpatterns = [
     path('myshow/', MyCustomUserShowView.as_view(), name='myshow'),
     path('show/<int:id>/', CustomUserShowView.as_view(), name='show'),
     path('list/', CustomUserViewSet.as_view({'get': 'list'}), name='list'),
-    path('signup/', CreateView.as_view(), name='create'),
-    path('updatepermission/<int:pk>/', CustomUserUpdatePermissionView.as_view(), name='updatepermission'),
-    path('forgetsendpassword/', ForgetSendPasswordResetView.as_view(), name='forgetpassword'),
-    path('forgetgetpassword/<uidb64>/<token>/', ForgetGetPasswordResetView.as_view(),name='forgetgetpassword'),
+    path('create/', CreateView.as_view(), name='create'),
     path('update/<int:pk>/', CustomUserUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', CustomUserDeleteView.as_view(), name='delete'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('changepassword/', CustomUserChangePasswordView.as_view(), name='change_password'),
 ]
