d407baf9c01f0770915c8da5b82bbe5e972386b8
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 14:32:31 +0330

    Update admin.py

--- IndustrialManagement_Backend/Projects/admin.py
+++ IndustrialManagement_Backend/Projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'nickname', 'start_date', 'end_date', 'real_start_date', 'real_end_date', 'external_members', 'owner', 'subOrganization', 'create_date')
+
+admin.site.register(Project, ProjectAdmin)
