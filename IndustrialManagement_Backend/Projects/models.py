d2cdd451ba966e3c4f081df2fa4fd4f4d014073f
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 14:32:35 +0330

    Update models.py

--- IndustrialManagement_Backend/Projects/models.py
+++ IndustrialManagement_Backend/Projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from SubOrganizations.models import SubOrganization, CustomUser
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    nickname = models.CharField(unique=True, max_length=50)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    real_start_date = models.DateTimeField(blank=True, null=True)
+    real_end_date = models.DateTimeField(blank=True, null=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    external_members = models.TextField(max_length=500)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    subOrganization = models.ForeignKey(SubOrganization, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
