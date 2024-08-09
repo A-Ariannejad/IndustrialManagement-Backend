23b3324cfde9e0558ecfc0cee0397e7477163428
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 16:35:21 +0330

    Update serializers.py

--- IndustrialManagement_Backend/CustomUserPermissions/serializers.py
+++ IndustrialManagement_Backend/CustomUserPermissions/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
 from .models import CustomUserPermission
 
-class PermissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUserPermission
-        fields = ['id', 'name', 'add_subOrganization', 'add_manager', 'add_project', 'user_access']
+
         
