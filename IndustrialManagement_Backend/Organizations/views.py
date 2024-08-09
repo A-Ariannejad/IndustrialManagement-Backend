a715177560f088fe4e6c016cefe94a212dc94976
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 12:52:19 +0330

    Update views.py

--- IndustrialManagement_Backend/Organizations/views.py
+++ IndustrialManagement_Backend/Organizations/views.py
@@ -6,26 +6,26 @@ from rest_framework import generics, viewsets
 class OrganizationShowView(generics.RetrieveAPIView):
     queryset = Organization.objects.all()
     serializer_class = GetOrganizationSerializer
-    permission_classes = [IsUserAccess]
+    # permission_classes = [IsUserAccess]
     lookup_field = 'id'
 
 class OrganizationViewSet(viewsets.ModelViewSet):
     queryset = Organization.objects.all().order_by('-create_date')
     serializer_class = GetOrganizationSerializer
-    permission_classes = [IsUserAccess]
+    # permission_classes = [IsUserAccess]
 
 class OrganizationCreateView(generics.CreateAPIView):
     queryset = Organization.objects.all()
     serializer_class = CreateOrganizationSerializer
-    permission_classes = [IsUserAccess]
+    # permission_classes = [IsUserAccess]
 
 class OrganizationUpdateView(generics.UpdateAPIView):
     queryset = Organization.objects.all()
     serializer_class = CreateOrganizationSerializer
-    permission_classes = [IsUserAccess]
+    # permission_classes = [IsUserAccess]
     
 class OrganizationDeleteView(generics.DestroyAPIView):
     queryset = Organization.objects.all()
     serializer_class = GetOrganizationSerializer
-    permission_classes = [IsUserAccess]
+    # permission_classes = [IsUserAccess]
     
