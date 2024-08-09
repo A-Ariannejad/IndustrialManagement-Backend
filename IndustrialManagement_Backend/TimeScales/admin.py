516855654de6e7b721b9cacf4dbfd4b67225daec
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 17:23:24 +0330

    Update admin.py

--- IndustrialManagement_Backend/TimeScales/admin.py
+++ IndustrialManagement_Backend/TimeScales/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TimeScale
 
-# Register your models here.
+class TimeScaleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project', 'program_progress_percentage', 'time_program_progress_percentage', 'date', 'create_date')
+
+admin.site.register(TimeScale, TimeScaleAdmin)
