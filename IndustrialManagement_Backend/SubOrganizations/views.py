4ee0300e5227c76dfec7b1a8810fe886c60b4842
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 13:48:11 +0330

    Update views.py

--- IndustrialManagement_Backend/SubOrganizations/views.py
+++ IndustrialManagement_Backend/SubOrganizations/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from .serializers import GetSubOrganizationSerializer, CreateSubOrganizationSerializer
+from CustomUserPermissions.views import IsUserAccess
+from .models import SubOrganization
+from rest_framework import generics, viewsets
 
-# Create your views here.
+class SubOrganizationShowView(generics.RetrieveAPIView):
+    queryset = SubOrganization.objects.all()
+    serializer_class = GetSubOrganizationSerializer
+    # permission_classes = [IsUserAccess]
+    lookup_field = 'id'
+
+class SubOrganizationViewSet(viewsets.ModelViewSet):
+    queryset = SubOrganization.objects.all().order_by('-create_date')
+    serializer_class = GetSubOrganizationSerializer
+    # permission_classes = [IsUserAccess]
+
+class SubOrganizationCreateView(generics.CreateAPIView):
+    queryset = SubOrganization.objects.all()
+    serializer_class = CreateSubOrganizationSerializer
+    # permission_classes = [IsUserAccess]
+
+class SubOrganizationUpdateView(generics.UpdateAPIView):
+    queryset = SubOrganization.objects.all()
+    serializer_class = CreateSubOrganizationSerializer
+    # permission_classes = [IsUserAccess]
+    
+class SubOrganizationDeleteView(generics.DestroyAPIView):
+    queryset = SubOrganization.objects.all()
+    serializer_class = GetSubOrganizationSerializer
+    # permission_classes = [IsUserAccess]
+    
