bdc80a1b9f8a99762576aee0f77d36291d90434e
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 12:52:13 +0330

    Update admin.py

--- IndustrialManagement_Backend/Organizations/admin.py
+++ IndustrialManagement_Backend/Organizations/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Organization
 
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'boss', 'create_date')
+    list_display = ('id', 'name', 'nickname', 'phone_number', 'postal_code', 'owner', 'create_date')
 
 admin.site.register(Organization, OrganizationAdmin)
