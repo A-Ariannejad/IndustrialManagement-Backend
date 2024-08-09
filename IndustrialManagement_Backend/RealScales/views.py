1fe212ca3d6aa244dc3b5b3351d9a08a7b9cc729
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 16:57:00 +0330

    Update views.py

--- IndustrialManagement_Backend/RealScales/views.py
+++ IndustrialManagement_Backend/RealScales/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .serializers import CreateRealScaleSerializer
+from IndustrialManagement_Backend.serializers import GetRealScaleSerializer
+from CustomUserPermissions.views import IsUserAccess
+from .models import RealScale
+from rest_framework import generics, viewsets
 
-# Create your views here.
+class RealScaleShowView(generics.RetrieveAPIView):
+    queryset = RealScale.objects.all()
+    serializer_class = GetRealScaleSerializer
+    # permission_classes = [IsUserAccess]
+    lookup_field = 'id'
+
+class RealScaleViewSet(viewsets.ModelViewSet):
+    queryset = RealScale.objects.all().order_by('-create_date')
+    serializer_class = GetRealScaleSerializer
+    # permission_classes = [IsUserAccess]
+
+class RealScaleCreateView(generics.CreateAPIView):
+    queryset = RealScale.objects.all()
+    serializer_class = CreateRealScaleSerializer
+    # permission_classes = [IsUserAccess]
+
+class RealScaleUpdateView(generics.UpdateAPIView):
+    queryset = RealScale.objects.all()
+    serializer_class = CreateRealScaleSerializer
+    # permission_classes = [IsUserAccess]
+    
+class RealScaleDeleteView(generics.DestroyAPIView):
+    queryset = RealScale.objects.all()
+    serializer_class = GetRealScaleSerializer
+    # permission_classes = [IsUserAccess]
+    
