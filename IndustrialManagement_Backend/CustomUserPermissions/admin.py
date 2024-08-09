94c4d81adeea91ec85263a434ee3ea2a1cfd069c
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 12:51:58 +0330

    Update admin.py

--- IndustrialManagement_Backend/CustomUserPermissions/admin.py
+++ IndustrialManagement_Backend/CustomUserPermissions/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import CustomUserPermission
 
 class CustomUserPermissionPerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'add_subOrganization', 'add_manager', 'add_project', 'user_access')
+    list_display = ('id', 'name', 'add_subOrganization', 'add_manager', 'add_project', 'user_access')
 
 admin.site.register(CustomUserPermission, CustomUserPermissionPerAdmin)
