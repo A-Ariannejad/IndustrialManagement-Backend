ba04c9a8cb01c72cbff20fc434026a21769d5889
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 16:35:30 +0330

    Update serializers.py

--- IndustrialManagement_Backend/CustomUsers/serializers.py
+++ IndustrialManagement_Backend/CustomUsers/serializers.py
@@ -1,18 +1,14 @@
 from rest_framework import serializers
 from .models import CustomUser
-from CustomUserPermissions.serializers import PermissionSerializer
-
-class GetCustomUserSerializer(serializers.ModelSerializer):
-    user_permissions = PermissionSerializer()
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'user_permissions']
+from IndustrialManagement_Backend.serializers import GetPermissionSerializer
+from IndustrialManagement_Backend.serializers import GetSubOrganizationSerializer
 
 class GetCustomUserProfileSerializer(serializers.ModelSerializer):
-    user_permissions = PermissionSerializer()
+    user_permissions = GetPermissionSerializer()
+    subOrganizations = GetSubOrganizationSerializer()
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'nickname', 'first_name', 'last_name', 'social_id_number', 'personal_id_number', 'mobile_phone_number',  'phone_number', 'education_level', 'create_date', 'user_permissions']
+        fields = ['id', 'username', 'nickname', 'first_name', 'last_name', 'social_id_number', 'personal_id_number', 'mobile_phone_number', 'phone_number', 'education_level', 'subOrganizations', 'create_date', 'user_permissions']
 
 class CreateCustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,5 +18,5 @@ class CreateCustomUserSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         ret = super().to_representation(instance)
         permissions = instance.user_permissions
-        ret['user_permissions'] = PermissionSerializer(permissions).data
+        ret['user_permissions'] = GetPermissionSerializer(permissions).data
         return ret
