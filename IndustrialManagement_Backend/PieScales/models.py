5e8b3fbd0ff2d71caae3d4642d37c880ee6625a4
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 19:45:56 +0330

    Update models.py

--- IndustrialManagement_Backend/PieScales/models.py
+++ IndustrialManagement_Backend/PieScales/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Projects.models import Project
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class PieScale(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    pending_percentage = models.DecimalField(max_digits=5, decimal_places=2, validators=[MaxValueValidator(100), MinValueValidator(0)], default=0.00) 
+    doing_percentage = models.DecimalField(max_digits=5, decimal_places=2, validators=[MaxValueValidator(100), MinValueValidator(0)], default=0.00) 
+    finished_percentage = models.DecimalField(max_digits=5, decimal_places=2, validators=[MaxValueValidator(100), MinValueValidator(0)], default=0.00) 
+    date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.project.name
