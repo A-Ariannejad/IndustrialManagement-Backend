0a86289092402cbd761d2202d077555fee378159
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 19:46:07 +0330

    Update views.py

--- IndustrialManagement_Backend/PieScales/views.py
+++ IndustrialManagement_Backend/PieScales/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .serializers import CreatePieScaleSerializer
+from IndustrialManagement_Backend.serializers import GetPieScaleSerializer
+from CustomUserPermissions.views import IsUserAccess
+from .models import PieScale
+from rest_framework import generics, viewsets
 
-# Create your views here.
+class PieScaleShowView(generics.RetrieveAPIView):
+    queryset = PieScale.objects.all()
+    serializer_class = GetPieScaleSerializer
+    # permission_classes = [IsUserAccess]
+    lookup_field = 'id'
+
+class PieScaleViewSet(viewsets.ModelViewSet):
+    queryset = PieScale.objects.all().order_by('-create_date')
+    serializer_class = GetPieScaleSerializer
+    # permission_classes = [IsUserAccess]
+
+class PieScaleCreateView(generics.CreateAPIView):
+    queryset = PieScale.objects.all()
+    serializer_class = CreatePieScaleSerializer
+    # permission_classes = [IsUserAccess]
+
+class PieScaleUpdateView(generics.UpdateAPIView):
+    queryset = PieScale.objects.all()
+    serializer_class = CreatePieScaleSerializer
+    # permission_classes = [IsUserAccess]
+    
+class PieScaleDeleteView(generics.DestroyAPIView):
+    queryset = PieScale.objects.all()
+    serializer_class = GetPieScaleSerializer
+    # permission_classes = [IsUserAccess]
+    
