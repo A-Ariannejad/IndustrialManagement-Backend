2f25a4277e06f22cf992a8998dab51afc6b8890d
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 17:23:33 +0330

    Update views.py

--- IndustrialManagement_Backend/TimeScales/views.py
+++ IndustrialManagement_Backend/TimeScales/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .serializers import CreateTimeScaleSerializer
+from IndustrialManagement_Backend.serializers import GetTimeScaleSerializer
+from CustomUserPermissions.views import IsUserAccess
+from .models import TimeScale
+from rest_framework import generics, viewsets
 
-# Create your views here.
+class TimeScaleShowView(generics.RetrieveAPIView):
+    queryset = TimeScale.objects.all()
+    serializer_class = GetTimeScaleSerializer
+    # permission_classes = [IsUserAccess]
+    lookup_field = 'id'
+
+class TimeScaleViewSet(viewsets.ModelViewSet):
+    queryset = TimeScale.objects.all().order_by('-create_date')
+    serializer_class = GetTimeScaleSerializer
+    # permission_classes = [IsUserAccess]
+
+class TimeScaleCreateView(generics.CreateAPIView):
+    queryset = TimeScale.objects.all()
+    serializer_class = CreateTimeScaleSerializer
+    # permission_classes = [IsUserAccess]
+
+class TimeScaleUpdateView(generics.UpdateAPIView):
+    queryset = TimeScale.objects.all()
+    serializer_class = CreateTimeScaleSerializer
+    # permission_classes = [IsUserAccess]
+    
+class TimeScaleDeleteView(generics.DestroyAPIView):
+    queryset = TimeScale.objects.all()
+    serializer_class = GetTimeScaleSerializer
+    # permission_classes = [IsUserAccess]
+    
