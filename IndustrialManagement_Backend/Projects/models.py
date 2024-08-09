af54630fb72804e2f854a7f20bbbad4d6d51ed2b
A_Ariannejad <ariannejad14@gmail.com>
2024-08-09 16:01:58 +0330

    Update models.py

--- IndustrialManagement_Backend/Projects/models.py
+++ IndustrialManagement_Backend/Projects/models.py
@@ -8,10 +8,10 @@ class Project(models.Model):
     end_date = models.DateTimeField(blank=True, null=True)
     real_start_date = models.DateTimeField(blank=True, null=True)
     real_end_date = models.DateTimeField(blank=True, null=True)
-    create_date = models.DateTimeField(auto_now_add=True)
-    external_members = models.TextField(max_length=500)
+    external_members = models.TextField(max_length=500, blank=True, null=True)
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     subOrganization = models.ForeignKey(SubOrganization, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
