d056c296368685ad9a7d82bf2ea300a08412167f
A_Ariannejad <ariannejad14@gmail.com>
2024-09-26 18:59:06 +0330

    Update views.py

--- IndustrialManagement_Backend/SubOrganizations/views.py
+++ IndustrialManagement_Backend/SubOrganizations/views.py
@@ -1,7 +1,9 @@
 from .serializers import CreateSubOrganizationSerializer
 from IndustrialManagement_Backend.serializers import GetSubOrganizationSerializer
+from Projects.models import CustomUser, Project
 from CustomUserPermissions.views import IsUserAccess
 from .models import SubOrganization
+from django.db.models import Prefetch
 from rest_framework import generics, viewsets
 
 class SubOrganizationShowView(generics.RetrieveAPIView):
@@ -11,7 +13,7 @@ class SubOrganizationShowView(generics.RetrieveAPIView):
     lookup_field = 'id'
 
 class SubOrganizationViewSet(viewsets.ModelViewSet):
-    queryset = SubOrganization.objects.all().order_by('-create_date')
+    queryset = SubOrganization.objects.prefetch_related(Prefetch('project_owners', queryset=CustomUser.objects.prefetch_related(Prefetch('projects', queryset=Project.objects.all())))).all().order_by('-create_date')
     serializer_class = GetSubOrganizationSerializer
     # permission_classes = [IsUserAccess]
 
