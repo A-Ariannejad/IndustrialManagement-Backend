fd9db92216899b6d574f770453c03d858ec54fb2
A_Ariannejad <ariannejad14@gmail.com>
2024-10-09 15:45:54 +0330

    Update urls.py

--- IndustrialManagement_Backend/SubOrganizations/urls.py
+++ IndustrialManagement_Backend/SubOrganizations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SubOrganizationCreateView, SubOrganizationViewSet, SubOrganizationUpdateView, SubOrganizationShowView, SubOrganizationDeleteView
+from .views import SubOrganizationCreateView, SubOrganizationViewSet, SubOrganizationUpdateView, SubOrganizationShowView, SubOrganizationDeleteView, SelectSubOrganizationViewSet
 
 urlpatterns = [
     path('show/<int:id>/', SubOrganizationShowView.as_view(), name='show'),
     path('list/', SubOrganizationViewSet.as_view({'get': 'list'}), name='list'),
+    path('selectlist/', SelectSubOrganizationViewSet.as_view({'get': 'list'}), name='selectlist'),
     path('create/', SubOrganizationCreateView.as_view(), name='create'),
     path('update/<int:pk>/', SubOrganizationUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', SubOrganizationDeleteView.as_view(), name='delete'),
