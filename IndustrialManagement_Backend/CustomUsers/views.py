49c0985767931a36c4aa3a5036d649f2070379a3
A_Ariannejad <ariannejad14@gmail.com>
2024-10-12 17:12:32 +0330

    Update views.py

--- IndustrialManagement_Backend/CustomUsers/views.py
+++ IndustrialManagement_Backend/CustomUsers/views.py
@@ -1,7 +1,7 @@
 from .serializers import GetCustomUserProfileSerializer, CreateCustomUserSerializer, UpdateCustomUserSerializer
 from IndustrialManagement_Backend.serializers import GetCustomUserSerializer
 from CustomUserPermissions.views import IsUserAccess
-from .models import CustomUser
+from .models import CustomUser, CustomUserPermission
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from rest_framework import generics, permissions, viewsets, status
@@ -46,12 +46,32 @@ class CustomUserUpdateView(generics.UpdateAPIView):
     serializer_class = UpdateCustomUserSerializer
     # permission_classes = [IsUserAccess]
     def perform_update(self, serializer):
+        new_username = self.request.data.get('username')
+        new_nickname = self.request.data.get('nickname')
+        new_education_level = self.request.data.get('education_level')
+        new_user_permissions = self.request.data.get('user_permissions')
         new_password = self.request.data.get('password')
-        if new_password:
-            serializer.save(password=make_password(self.request.data.get('password')))
+        user = CustomUser.objects.filter(id=self.kwargs['pk']).first()
+        serializer.user_permissions = user.user_permissions
+        if not new_username: 
+            new_username = user.username
+        if not new_nickname: 
+            new_nickname = user.nickname
+        if not new_education_level: 
+            new_education_level = user.education_level
+        if not new_user_permissions: 
+            new_user_permissions = user.user_permissions
         else:
-            user = CustomUser.objects.filter(id=self.kwargs['pk']).first()
-            serializer.save(password=user.password)
+            permission = CustomUserPermission.objects.filter(id=new_user_permissions).first()
+            if permission:
+                serializer.user_permissions = permission
+            else:
+                new_user_permissions = user.user_permissions
+        if new_password:
+            new_password=make_password(self.request.data.get('password'))
+        else:
+            new_password=user.password
+        serializer.save(username=new_username, nickname=new_nickname, education_level=new_education_level, user_permissions=new_user_permissions, password=new_password)
     
 class CustomUserDeleteView(generics.DestroyAPIView):
     queryset = CustomUser.objects.all()
