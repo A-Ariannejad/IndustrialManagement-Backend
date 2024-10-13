bf361eeb87b7e77507888f245d4a11b4e867a00c
Arshia Ariannejad <ariannejad14@gmail.com>
2024-10-13 15:09:02 +0330

    Update views.py

--- IndustrialManagement_Backend/RealScales/views.py
+++ IndustrialManagement_Backend/RealScales/views.py
@@ -1,4 +1,4 @@
-from .serializers import CreateRealScaleSerializer
+from .serializers import CreateRealScaleSerializer, GetRealScaleWithoutProjectSerializer
 from IndustrialManagement_Backend.serializers import GetRealScaleSerializer
 from CustomUserPermissions.views import IsUserAccess
 from .models import RealScale
@@ -14,6 +14,14 @@ class RealScaleViewSet(viewsets.ModelViewSet):
     queryset = RealScale.objects.all().order_by('-create_date')
     serializer_class = GetRealScaleSerializer
     # permission_classes = [IsUserAccess]
+
+class RealScaleShowByProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = GetRealScaleWithoutProjectSerializer
+    # permission_classes = [IsUserAccess]
+
+    def get_queryset(self):
+        queryset = RealScale.objects.filter(project=self.kwargs['pk']).all().order_by('-create_date')
+        return queryset
 
 class RealScaleCreateView(generics.CreateAPIView):
     queryset = RealScale.objects.all()
