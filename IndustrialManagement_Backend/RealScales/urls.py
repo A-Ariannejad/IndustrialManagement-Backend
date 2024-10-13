70b889c5fd367bec7043c15c9d04d5b70cd65af5
Arshia Ariannejad <ariannejad14@gmail.com>
2024-10-13 15:09:00 +0330

    Update urls.py

--- IndustrialManagement_Backend/RealScales/urls.py
+++ IndustrialManagement_Backend/RealScales/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RealScaleCreateView, RealScaleViewSet, RealScaleUpdateView, RealScaleShowView, RealScaleDeleteView
+from .views import RealScaleCreateView, RealScaleViewSet, RealScaleUpdateView, RealScaleShowView, RealScaleDeleteView, RealScaleShowByProjectViewSet
 
 urlpatterns = [
     path('show/<int:id>/', RealScaleShowView.as_view(), name='show'),
+    path('showbyproject/<int:pk>/', RealScaleShowByProjectViewSet.as_view({'get': 'list'}), name='showbyproject'),
     path('list/', RealScaleViewSet.as_view({'get': 'list'}), name='list'),
     path('create/', RealScaleCreateView.as_view(), name='create'),
     path('update/<int:pk>/', RealScaleUpdateView.as_view(), name='update'),
