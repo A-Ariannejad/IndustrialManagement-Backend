f94ea5bab3b26f3f65d113e37d682518f57a55f7
Arshia Ariannejad <ariannejad14@gmail.com>
2024-10-13 15:08:55 +0330

    Update urls.py

--- IndustrialManagement_Backend/PieScales/urls.py
+++ IndustrialManagement_Backend/PieScales/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PieScaleCreateView, PieScaleViewSet, PieScaleUpdateView, PieScaleShowView, PieScaleDeleteView
+from .views import PieScaleCreateView, PieScaleViewSet, PieScaleUpdateView, PieScaleShowView, PieScaleDeleteView, PieScaleShowByProjectViewSet
 
 urlpatterns = [
     path('show/<int:id>/', PieScaleShowView.as_view(), name='show'),
+    path('showbyproject/<int:pk>/', PieScaleShowByProjectViewSet.as_view({'get': 'list'}), name='showbyproject'),
     path('list/', PieScaleViewSet.as_view({'get': 'list'}), name='list'),
     path('create/', PieScaleCreateView.as_view(), name='create'),
     path('update/<int:pk>/', PieScaleUpdateView.as_view(), name='update'),
