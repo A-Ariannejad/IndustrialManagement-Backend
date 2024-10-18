565e1928a32f208b0a59699015a1c23c63886d34
A_Ariannejad <ariannejad14@gmail.com>
2024-10-19 02:31:35 +0330

    Create permissions.py

---
+++ IndustrialManagement_Backend/CustomUsers/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission
+from CustomUsers.models import LogicUser
+from rest_framework import status
+from IndustrialManagement_Backend.serializers import CustomValidation
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        user = LogicUser.get_user(request = request)
+        if user:
+            if user.admin:
+                return True
+        raise CustomValidation("شما اجازه این کار را ندارید", "", status_code=status.HTTP_401_UNAUTHORIZED)
+    
+class IsSuperAdmin(BasePermission):
+    def has_permission(self, request, view):
+        user = LogicUser.get_user(request = request)
+        if user:
+            if user.is_superuser:
+                return True
+        raise CustomValidation("شما اجازه این کار را ندارید", "", status_code=status.HTTP_401_UNAUTHORIZED)
