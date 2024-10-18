4320658ab7b045ceab3fc71f9e73207c51e48336
A_Ariannejad <ariannejad14@gmail.com>
2024-10-19 02:32:20 +0330

    Update views.py

--- IndustrialManagement_Backend/RealScales/views.py
+++ IndustrialManagement_Backend/RealScales/views.py
@@ -1,7 +1,6 @@
 from .serializers import CreateRealScaleSerializer, GetRealScaleWithoutProjectSerializer
 from IndustrialManagement_Backend.serializers import GetRealScaleSerializer
-from CustomUserPermissions.views import IsUserAccess
-from .models import RealScale
+from .models import RealScale, Project
 from rest_framework import generics, viewsets
 
 class RealScaleShowView(generics.RetrieveAPIView):
@@ -20,13 +19,30 @@ class RealScaleShowByProjectViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsUserAccess]
 
     def get_queryset(self):
-        queryset = RealScale.objects.filter(project=self.kwargs['pk']).all().order_by('-create_date')
-        return queryset
+        user = self.request.user
+        project_id = self.kwargs['pk']
+        if user.admin:
+            return RealScale.objects.filter(project=project_id).order_by('-create_date')
+        project = Project.objects.filter(id=project_id).first()
+        if not project:
+            return RealScale.objects.none()
+        if project.owner == user:
+            return RealScale.objects.filter(project=project).order_by('-create_date')
+        if project.subOrganization.owner == user:
+            return RealScale.objects.filter(project=project).order_by('-create_date')
+        if project in user.projects.all() and user.crud_project:
+            return RealScale.objects.filter(project=project).order_by('-create_date')
+        return RealScale.objects.none()
 
 class RealScaleCreateView(generics.CreateAPIView):
     queryset = RealScale.objects.all()
     serializer_class = CreateRealScaleSerializer
     # permission_classes = [IsUserAccess]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
 
 class RealScaleUpdateView(generics.UpdateAPIView):
     queryset = RealScale.objects.all()
