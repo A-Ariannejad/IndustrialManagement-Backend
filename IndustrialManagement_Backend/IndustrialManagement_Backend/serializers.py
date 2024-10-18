7a0ccfeef9d84c52309419914102a06f3a81adce
A_Ariannejad <ariannejad14@gmail.com>
2024-10-19 02:31:27 +0330

    Update serializers.py

--- IndustrialManagement_Backend/IndustrialManagement_Backend/serializers.py
+++ IndustrialManagement_Backend/IndustrialManagement_Backend/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework import serializers
 from rest_framework.exceptions import APIException
-from CustomUserPermissions.models import CustomUserPermission
 from CustomUsers.models import CustomUser
 from Organizations.models import Organization
 from SubOrganizations.models import SubOrganization
@@ -10,6 +9,7 @@ from RealScales.models import RealScale
 from TimeScales.models import TimeScale
 from PieScales.models import PieScale
 from ProjectFiles.models import ProjectFile
+from django.db.models import Q
 
 class CustomValidation(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -21,54 +21,70 @@ class CustomValidation(APIException):
             self.detail = detail
         else: self.detail = self.default_detail
 
-class GetPermissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUserPermission
-        fields = ['id', 'name', 'add_subOrganization', 'add_manager', 'add_project', 'user_access']
-
 class GetCustomUserSerializer(serializers.ModelSerializer):
-    user_permissions = GetPermissionSerializer()
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'user_permissions']
+        fields = ['id', 'username', 'admin', 'crud_project', 'projects']
 
 class GetOrganizationSerializer(serializers.ModelSerializer):
-    owner = GetCustomUserSerializer()
     class Meta:
         model = Organization
         fields = '__all__'
 
+    def get_sub_organs(self, instance):
+        request_user = self.context['request'].user
+        sub_organizations = instance.suborganization_set.all()
+        if not request_user.admin:
+            sub_organizations = sub_organizations.filter(
+                Q(owner=request_user) |
+                Q(project__owner=request_user) |
+                Q(project__in=request_user.projects.all())
+            ).distinct()
+        return GetSubOrganizationSerializer(sub_organizations, many=True, context={'request': self.context['request']}).data
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['sub_organs'] = self.get_sub_organs(instance)
+        return ret
+    
 class GetCustomUser_ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
 
 class GetSubOrganization_CustomUserSerializer(serializers.ModelSerializer):
-    user_permissions = GetPermissionSerializer()
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'user_permissions', 'education_level', 'mobile_phone_number']
+        fields = ['id', 'username', 'education_level', 'mobile_phone_number']
 
     def to_representation(self, instance):
+        request_user = self.context['request'].user
+        sub_org = self.context['sub_org']
         ret = super().to_representation(instance)
-        ret['projects'] = GetCustomUser_ProjectSerializer(instance.project_set.all(), many=True).data
+        if request_user.admin or instance == request_user:
+            ret['projects'] = GetCustomUser_ProjectSerializer(instance.project_set.filter(subOrganization=sub_org), many=True).data
+        else:
+            filtered_projects = instance.project_set.filter(Q(subOrganization=sub_org) & (Q(owner=request_user) | Q(id__in=request_user.projects.all())))
+            ret['projects'] = GetCustomUser_ProjectSerializer(filtered_projects, many=True).data
         return ret
-
 
 class GetSubOrganizationSerializer(serializers.ModelSerializer):
     owner = GetCustomUserSerializer()
-    organization = GetOrganizationSerializer()
+    # organization = GetOrganizationSerializer()
     
     class Meta:
         model = SubOrganization
         fields = '__all__'
     
     def to_representation(self, instance):
+        request_user = self.context['request'].user
         ret = super().to_representation(instance)
         users = instance.customuser_set.all()
-        ret['people'] = GetSubOrganization_CustomUserSerializer(users, many=True).data
-        ret['owner'] = GetCustomUserSerializer(instance.owner).data
-        ret['organization'] = GetOrganizationSerializer(instance.organization).data
+        if request_user.admin or instance.owner == request_user:
+            ret['people'] = GetSubOrganization_CustomUserSerializer(users, many=True, context={'request': self.context['request'], 'sub_org': instance}).data
+        else:
+            filtered_users = users.filter(Q(id=request_user.id) | Q(project__subOrganization=instance, project__id__in=request_user.projects.all())).distinct()
+            ret['people'] = GetSubOrganization_CustomUserSerializer(filtered_users, many=True, context={'request': self.context['request'], 'sub_org': instance}).data
         return ret
     
 class GetSelectSubOrganizationSerializer(serializers.ModelSerializer):
