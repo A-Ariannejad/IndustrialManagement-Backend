2af9bc449e9b51e649a3518f5ec40e4782b3fb86
A_Ariannejad <ariannejad14@gmail.com>
2024-10-19 02:31:33 +0330

    Update serializers.py

--- IndustrialManagement_Backend/CustomUsers/serializers.py
+++ IndustrialManagement_Backend/CustomUsers/serializers.py
@@ -1,40 +1,28 @@
 from rest_framework import serializers
-from .models import CustomUser, CustomUserPermission
-from IndustrialManagement_Backend.serializers import GetPermissionSerializer
-from IndustrialManagement_Backend.serializers import GetSubOrganizationSerializer
-from SubOrganizations.models import SubOrganization
+from .models import CustomUser
+from IndustrialManagement_Backend.serializers import GetSubOrganizationSerializer, GetProjectSerializer
+from Projects.models import SubOrganization, Project
 
 class GetCustomUserProfileSerializer(serializers.ModelSerializer):
-    user_permissions = GetPermissionSerializer()
     subOrganizations = GetSubOrganizationSerializer()
+    projects = GetProjectSerializer(many=True)
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'nickname', 'first_name', 'last_name', 'social_id_number', 'personal_id_number', 'mobile_phone_number', 'phone_number', 'education_level', 'subOrganizations', 'create_date', 'user_permissions']
+        fields = ['id', 'username', 'nickname', 'first_name', 'last_name', 'social_id_number', 'personal_id_number', 'mobile_phone_number', 'phone_number', 'education_level', 'admin', 'crud_project', 'projects', 'subOrganizations', 'create_date']
 
 class CreateCustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'nickname', 'first_name', 'password', 'last_name', 'social_id_number', 'personal_id_number', 'mobile_phone_number',  'phone_number', 'education_level', 'subOrganizations', 'create_date', 'user_permissions']
-    
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        permissions = instance.user_permissions
-        ret['user_permissions'] = GetPermissionSerializer(permissions).data
-        return ret
+        fields = ['id', 'username', 'nickname', 'first_name', 'password', 'last_name', 'social_id_number', 'personal_id_number', 'mobile_phone_number', 'phone_number', 'education_level', 'admin', 'crud_project', 'projects', 'subOrganizations', 'create_date']
     
 class UpdateCustomUserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(required=False, allow_blank=True, allow_null=True)  # Allow blank and null for username
-    nickname = serializers.CharField(required=False, allow_blank=True, allow_null=True)  # Allow blank and null for nickname
-    education_level = serializers.ChoiceField(choices=CustomUser.EDUCATION_CHOICES, required=False, allow_blank=True)  # Allow blank for education_level
-    user_permissions = serializers.PrimaryKeyRelatedField(queryset=CustomUserPermission.objects.all(), required=False, allow_null=True)  # Allow null for user_permissions
-    subOrganizations = serializers.PrimaryKeyRelatedField(queryset=SubOrganization.objects.all(), required=False, allow_null=True)  # Allow null for user_permissions
+    username = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    nickname = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    education_level = serializers.ChoiceField(choices=CustomUser.EDUCATION_CHOICES, required=False, allow_blank=True)
+    subOrganizations = serializers.PrimaryKeyRelatedField(queryset=SubOrganization.objects.all(), required=False, allow_null=True)
+    projects = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all(), required=False, allow_null=True, many=True)
     password = serializers.CharField(required=False, allow_blank=True, write_only=True)
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'nickname', 'first_name', 'last_name', 'password', 'social_id_number', 'personal_id_number', 'mobile_phone_number',  'phone_number', 'education_level', 'subOrganizations', 'create_date', 'user_permissions']
+        fields = ['id', 'username', 'nickname', 'first_name', 'last_name', 'password', 'social_id_number', 'personal_id_number', 'mobile_phone_number', 'phone_number', 'education_level', 'admin', 'crud_project', 'projects', 'subOrganizations', 'create_date']
     
-    def to_representation(self, instance):
-        ret = super().to_representation(instance)
-        permissions = instance.user_permissions
-        ret['user_permissions'] = GetPermissionSerializer(permissions).data
-        return ret
