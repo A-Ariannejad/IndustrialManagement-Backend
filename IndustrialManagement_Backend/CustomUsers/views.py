b74d8b84fe2bfe1c8bc64ee8b44625e34c6f237b
A_Ariannejad <ariannejad14@gmail.com>
2024-10-22 17:35:16 +0330

    Update views.py

--- IndustrialManagement_Backend/CustomUsers/views.py
+++ IndustrialManagement_Backend/CustomUsers/views.py
@@ -14,8 +14,13 @@ class MyCustomUserShowView(generics.RetrieveAPIView):
             user = CustomUser.objects.filter(id=request.user.id).first()
             if not user:
                 return Response({'error': 'User does not exist.'}, status=status.HTTP_404_NOT_FOUND)
-            serializer = self.serializer_class(user)
+            serializer = self.serializer_class(user, context=self.get_serializer_context())
             return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
 
 class CustomUserShowView(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
@@ -32,6 +37,11 @@ class CustomUserAdminViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all().order_by('-create_date')
     serializer_class = GetCustomUserProfileSerializer
     permission_classes = [permissions.IsAuthenticated, IsSuperAdmin]
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
 
 class CreateView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
@@ -52,6 +62,12 @@ class CustomUserUpdateView(generics.UpdateAPIView):
         new_education_level = self.request.data.get('education_level')
         new_password = self.request.data.get('password')
         user = CustomUser.objects.filter(id=self.kwargs['pk']).first()
+        x = CustomUser.objects.filter(username=new_username).first()
+        if x and x != user:
+            raise CustomValidation("کاربر با این نام کابری وجود دارد", "", status_code=status.HTTP_400_BAD_REQUEST)
+        z = CustomUser.objects.filter(nickname=new_nickname).first()
+        if z and z != user:
+            raise CustomValidation("کاربر با این نام مستعار وجود دارد", "", status_code=status.HTTP_400_BAD_REQUEST)
         if not new_username: 
             new_username = user.username
         if not new_nickname: 
